1e0c492d3bc1ec3795371be4e1ffdd1c519a8613
David <jd.monsalve@uniandes.edu.co>
2022-02-20 14:35:51 -0500

    CORS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var cors = require('cors')
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 mu = require('./lib/utils/mongo.js');
@@ -11,6 +12,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
